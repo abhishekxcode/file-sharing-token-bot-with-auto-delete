a6a006b2b793259ee2b5bc1e9aa51d9783c33eb0
Abhishek Pal <143728150+abhishekxcode@users.noreply.github.com>
2024-04-17 22:11:49 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,5 +1,3 @@
-#ultroidofficial : YT
-
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
@@ -10,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://youtu.be/BeNBEYc-q7Y'>Click here</a>\n○ Channel : @ultroid_official\n○ Support Group : @ultroidofficial_chat</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/zxninjas'>Click here</a>\n○ Channel : @zxninjas\n○ Support Group : @zxninjas</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
